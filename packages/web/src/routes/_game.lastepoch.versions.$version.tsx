2aa2144d9c0dfed5f52e988f3ba5d3082ce11136
Musholic <musholic.tbg@gmail.com>
2025-08-06 16:07:37 +0200

    Delete obsolete /lastepoch routes

--- packages/web/src/routes/_game.lastepoch.versions.$version.tsx
+++
@@ -1,6 +0,0 @@
-import PoBController from "../components/PoBController";
-import type { Route } from "./+types/_game.le.versions.$version";
-
-export default function (p: Route.ComponentProps) {
-  return <PoBController game="le" version={p.params.version} isHead={false} />;
-}
